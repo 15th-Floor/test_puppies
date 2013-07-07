bc62f8b190c481020243d8a8662b7f26b185c260
Jae <jae.han08@gmail.com>
2013-07-06 22:30:35 -0400

    adding adopting puppy steps

--- features/step_definitions/adopting_puppy_steps.rb
+++ features/step_definitions/adopting_puppy_steps.rb
@@ -1,5 +1,6 @@
 Given(/^I am on the puppy adoption site$/) do
   @browser.goto "http://puppies.herokuapp.com"
+  sleep 3
 end
 
 When(/^I click the View Details button$/) do
@@ -7,31 +8,31 @@ When(/^I click the View Details button$/) do
 end
 
 When(/^I click the Adopt Me button$/) do
-  pending # express the regexp above with the code you wish you had
+  @browser.button(:value => "Adopt Me!").click
 end
 
 When(/^I click the Complete the Adoption button$/) do
-  pending # express the regexp above with the code you wish you had
+  @browser.button(:value => "Complete the Adoption").click
 end
 
-When(/^I enter "(.*?)" in the name field$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I enter "(.*?)" in the name field$/) do |name|
+  @browser.text_field(:id => "order_name").set(name)
 end
 
-When(/^I enter "(.*?)" in the address field$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I enter "(.*?)" in the address field$/) do |address|
+  @browser.text_field(:id => "order_address").set(address)
 end
 
-When(/^I enter "(.*?)" in the email field$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I enter "(.*?)" in the email field$/) do |email|
+  @browser.text_field(:id => "order_email").set(email)
 end
 
-When(/^I select "(.*?)" from the pay with dropdown$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I select "(.*?)" from the pay with dropdown$/) do |pay_type|
+  @browser.select_list(:id => "order_pay_type").select(pay_type)
 end
 
 When(/^I click the Place Order button$/) do
-  pending # express the regexp above with the code you wish you had
+  @browser.button(:value => "Place Order").click
 end
 
 Then(/^I should see "(.*?)"$/) do |expected|
